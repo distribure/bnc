a455fd46cee95f14a301d23ad6a4f44ee3d3e535
Rajarshi Bhadra <17759435+Irajarshibhadra@users.noreply.github.com>
2018-07-29 12:36:39 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+include("header.php");
+?>
+<div class="jumbotron text-center">
+      <h1>BNGCSU</h1>
+      <p>The website of Students' Union of Bidhannagar College</p>
+ </div>
+ <?php
+ include("footer.php");
+ ?>
