7e43b5125b9ea403cce33da92279ad809f34be69
Rajarshi Bhadra <17759435+Irajarshibhadra@users.noreply.github.com>
2018-07-29 10:34:57 +0100

    Update header.php
    Separated the menu navbar portion

--- header.php
+++ header.php
@@ -5,37 +5,20 @@
     <head>
       
         <?php
-	    include(title.php);
+	    include("title.php");
 	?>
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1">
       <?php
-	    include(css_js.php)
+	    include("css_js.php");
 	?>  
 	</head>
+	    
+	    
      <body>
-        <div id="myPage" data-spy="scroll" data-target=".navbar" data-offset="60">
-     
-    <nav class="navbar navbar-default navbar-fixed-top">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>                        
-          </button>
-          <a class="navbar-brand" href="#myPage">Logo</a>
-        </div>
-        <div class="collapse navbar-collapse" id="myNavbar">
-          <ul class="nav navbar-nav navbar-right">
-            <li><a href="#about">ABOUT</a></li>
-            <li><a href="#services">SERVICES</a></li>
-            <li><a href="#portfolio">PORTFOLIO</a></li>
-            <li><a href="#pricing">PRICING</a></li>
-            <li><a href="#contact">CONTACT</a></li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+    <div id="myPage" data-spy="scroll" data-target=".navbar" data-offset="60">
+     <?php
+		    include("menu.php");
+	?>
      
 
