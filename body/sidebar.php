c6ae3d65616630dcdbbc933ced1eb5ade6e1f062
Rajarshi Bhadra <17759435+Irajarshibhadra@users.noreply.github.com>
2018-08-03 15:51:54 +0100

    Create sidebar.php

---
+++ body/sidebar.php
@@ -0,0 +1,14 @@
+<div class="col-sm-4">
+    <div class="well">
+      <p>Some text..</p>
+    </div>
+    <div class="well">
+       <p>Upcoming Events..</p>
+    </div>
+    <div class="well">
+       <p>Visit Our Blog</p>
+    </div>
+  </div>
+</div>
+<hr>
+</div>
