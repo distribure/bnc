cd3abda910f920d6fc142f2d72faa58c4a1e0040
Rajarshi Bhadra <17759435+Irajarshibhadra@users.noreply.github.com>
2018-08-03 15:30:47 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,15 @@
 <?php
 include("header.php");
 ?>
-<div class="jumbotron text-center"style = " background-image:url('images/18275151_160797477786163_1884934990410867511_n.jpg');
-background-attachment: fixed; background-position: center; background-repeat: no-repeat; background-size: cover; " >
+<div class="jumbotron text-center" >
       
       <h1 style="color='white'">BNGCSU</h1>
       <p style="color='white'">The website of Students' Union of Bidhannagar College</p>
  </div>
+<br />
+<?php
+include("slider.php");
+?>
  <?php
  include("footer.php");
  ?>
