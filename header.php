615b98f152e2181cbb454c2960a8b94258f52fe0
Rajarshi Bhadra <17759435+Irajarshibhadra@users.noreply.github.com>
2018-08-03 15:33:07 +0100

    Update header.php

--- header.php
+++ header.php
@@ -4,21 +4,21 @@
     <html lang="en">
     <head>
       
-        <?php
-	    include("/header/title.php");
-	?>
+<?php
+	    include("header/title.php");
+?>
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1">
-      <?php
-	    include("/header/css_js.php");
-	?>  
+<?php
+	    include("header/css_js.php");
+?>  
 	</head>
 	    
 	    
      <body>
     <div id="myPage" data-spy="scroll" data-target=".navbar" data-offset="60">
-     <?php
-		    include("/header/menu.php");
-	?>
+<?php
+	    include("header/menu.php");
+?>
      
 
